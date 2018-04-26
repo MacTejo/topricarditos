8ef135340fc3f88f77f87b9ca3b8ae8016bfcfe7
Luis Ramirez <me.luisramirez@gmail.com>
2018-04-27 00:41:03 +0200

    Update dependency with vulnerability

--- index.php
+++ index.php
@@ -9,6 +9,29 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 ?>
 
+<?php
+$token = "YOUR_TOKEN_HERE";
+$opts = [
+    "http" => [
+        "header" => "auth:" . $token
+    ]
+];
+
+$context = stream_context_create($opts);
+
+// Add your clan URL
+$test = file_get_contents("http://api.cr-api.com/clan/22CRJCRC", true, $context);
+
+$test = preg_replace('/\\\\\"/', "\"", $test);
+
+$test = str_replace("'", "", $test);
+
+// All data save on top-ricarditos.json
+$json_data = urldecode(stripslashes($test));
+file_put_contents('clan.json', $json_data);
+
+?>
+
 <!DOCTYPE html>
 <html>
 
